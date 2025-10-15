63c5129d11d5f3373595de5062104921b8740e66
kenyacrawford19-cpu <kenyacrawford19@gmail.com>
2025-10-15 15:09:36 -0400

    new input

--- src/main/java/com/pluralsight/FinancialLedger.java
+++ src/main/java/com/pluralsight/FinancialLedger.java
@@ -6,51 +6,138 @@ import java.awt.*;
 import java.sql.Date;
 import java.time.LocalDate;
 import java.time.LocalDateTime;
+import java.time.LocalTime;
 import java.time.format.DateTimeFormatter;
 import java.util.Scanner;
 
 public class FinancialLedger {
-  public void showMainScreen(){
+    public void showMainScreen(){
+      Scanner scanner = new Scanner(System.in);
+
       System.out.println("This is the home screen");
       //show menu options
-      public static void display() {
-          boolean running = true;
+      //public static void display() {
+      boolean running = true;
 
-          while (running) {
-              System.out.println("===============");
-              System.out.println("Welcome to DDSG Financial Ledger");
-              System.out.println("===============");
-              System.out.println("D) Add Deposit");
-              System.out.println("P) Make Payment (Debit) ");
-              System.out.println("L) Ledger");
-              System.out.println("X) Exiting DDSG Financial Ledger");
-              System.out.println("---------------");
-              System.out.println("Enter your choice: ");
-              String choice = scanner.nextLine().trim().toUpperCase();
+      while (running) {
+          System.out.println("===============");
+          System.out.println("Welcome to DDSG Financial Ledger");
+          System.out.println("===============");
+          System.out.println("D) Add Deposit");
+          System.out.println("P) Make Payment (Debit) ");
+          System.out.println("L) Ledger");
+          System.out.println("X) Exiting DDSG Financial Ledger");
+          System.out.println("---------------");
 
-      //user selects one of the options (requires the scanner)
-      public class FinancialLedger{
-          private static Scanner scanner = new Scanner(System.in);
-          private static TransactionService transactionService = new Transacation();
-      }
-      //use switch statement to run the user choice
-      switch (choice) {
-          case "D":
-              transactionService.addDeposit();
-              break;
-          case "P":
-              transactionService.makePayment();
-              break;
-          case "L":
-              LedgerEntry.display();
-              break;
-          case "X":
-              System.out.println("Exiting...Goodbye");
-              break;
+          //user selects one of the options (requires the scanner)
+          System.out.println("Enter your choice: ");
+          String choice = scanner.nextLine().trim().toUpperCase();
+
+          //public class FinancialLedger{
+
+          //use switch statement to run the user choice
+          switch (choice) {
+              case "D":
+                  addDeposit();
+                  break;
+              case "P":
+                  makePayment();
+                  break;
+              case "L":
+                  showLedgerScreen();
+                  break;
+              case "X":
+                  System.out.println("Exiting...Goodbye");
+                  break;
+          }
       }
   }
 
-    //DateTime Formatter
-    DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yyyy-MM-DD : mm:ss");
+    public void addDeposit(){
+        Scanner scanner = new Scanner(System.in);
+        //LocalDate Date, LocalTime Time, String description, String Vendor, double amount
+        LocalDate date = LocalDate.now();
+        LocalTime time = LocalTime.now();
 
+        System.out.println("Description: ");
+        String description = scanner.nextLine();
+        System.out.println("Vendor: ");
+        String vendor = scanner.nextLine();
+        System.out.println("Amount: ");
+        double amount = Double.parseDouble(scanner.nextLine());
+        LedgerEntry ledger = new LedgerEntry(date, time, description, vendor, amount);
+        String ledgerEntry = ledger.toString();
+        // write the ledgerEntry into your CSV
+    }
+
+    public void makePayment(){
+        Scanner scanner = new Scanner(System.in);
+        //LocalDate Date, LocalTime Time, String description, String Vendor, double amount
+        LocalDate date = LocalDate.now();
+        LocalTime time = LocalTime.now();
+
+        System.out.println("Description: ");
+        String description = scanner.nextLine();
+        System.out.println("Vendor: ");
+        String vendor = scanner.nextLine();
+        System.out.println("Amount: ");
+        double amount = -1 * Double.parseDouble(scanner.nextLine());
+        LedgerEntry ledger = new LedgerEntry(date, time, description, vendor, amount);
+        String ledgerEntry = ledger.toString();
+        // write the ledgerEntry into your CSV
+    }
+
+    public void showLedgerScreen() {
+        String File_Name = "transactions.csv";
+        DateTimeFormatter DATE_FORMAT = DateTimeFormatter.ofPattern("yyyy-MM-dd");
+        Scanner scanner = new Scanner(System.in);
+        //LedgerEntry ledgerEntry = new LedgerEntry();
+
+            while (true) {
+                System.out.println("\nLedger");
+                System.out.println("--------------------");
+                System.out.println("A) All Entries");
+                System.out.println("D) Deposits Only");
+                System.out.println("P) Payments Only");
+                System.out.println("R) Reports");
+                System.out.println("0) Back To Home");
+                System.out.println("-------------------");
+                System.out.println("Please select an option: ");
+
+                String choice = scanner.nextLine().trim().toUpperCase();
+
+                switch (choice) {
+                    case "A" -> showAllEntries();
+                    case "D" -> showDeposits();
+                    case "P" -> showPayments();
+                    case "R" -> showReportsMenu();
+                    case "0" -> {
+                        System.out.println("Returning to FinancialLedger Screen...");
+                        return;
+
+                    }
+                    default -> System.out.println("Invalid choice. Try again.");
+
+                }
+
+            }
+        }
+
+    private static void showPayments() {
+
+    }
+
+    private static void showReportsMenu() {
+    }
+
+    private static void showAllEntries() {
+    }
+
+    private static void showDeposits() {
+    }
+
+    public void showReportScreen(){
+
+    }
 }
+
--- src/main/java/com/pluralsight/LedgerEntry.java
+++ src/main/java/com/pluralsight/LedgerEntry.java
@@ -18,7 +18,7 @@ public class LedgerEntry {
     //Vendor:String
     String Vendor;
     //Price:Double
-    Double amount;
+    double amount;
 
     // Constructors
     public LedgerEntry(LocalDate Date, LocalTime Time, String description, String Vendor, double amount) {
@@ -45,63 +45,9 @@ public class LedgerEntry {
     public String toString() {
         return String.format("%s | %s | %-20s | %-15s | %10.2f",
                 Date,Time,Description, Vendor, amount);
-
-
-
-
-
-
     }
 }
 
-public class LedgerEntry{
-    private static final String File_Name = "transactions.csv";
-    private static final DateTimeFormatter DATE_FORMAT = DateTimeFormatter.ofPattern("yyyy-MM-dd");
-    private static final Scanner scanner = new Scanner(System.in);
 
-
-    private static void showLedgerEntry() {
-        while (true) {
-            System.out.println("\nLedger");
-            System.out.println("--------------------");
-            System.out.println("A) All Entries");
-            System.out.println("D) Deposits Only");
-            System.out.println("P) Payments Only");
-            System.out.println("R) Reports");
-            System.out.println("0) Back To Home");
-            System.out.println("-------------------");
-            System.out.println("Please select an option: ");
-
-            String choice = scanner.nextInt().trim().toUpperCase();
-            switch (choice) {
-                case "A" -> showAllEntries();
-                case "D" -> showDeposits();
-                case "P" -> showPayments();
-                case "R" -> showReportsMenu();
-                case "0" -> {
-                    System.out.println("Returning to FinancialLedger Screen...");
-                    return;
-
-                }
-                default -> System.out.println("Invalid choice. Try again.");
-
-            }
-
-        }
-    }
-
-    private static void showPayments() {
-
-    }
-
-    private static void showReportsMenu() {
-    }
-
-    private static void showAllEntries() {
-    }
-
-    private static void showDeposits() {
-    }
-}
 
 
